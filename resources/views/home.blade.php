00477cf97427be412751bd99d8af1e3ae7a2a9c4
j9k3r <j9k3r@yandex.ru>
2025-03-14 12:50:17 +0300

    Добавлена сортировка по порядку, исправлено сохранение примененных фильтров

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,15 @@
 <h1>Список товаров</h1>
 <form method="GET">
     <label for="sort_by">Сортировать по:</label>
+    <input type="hidden" name="group_id" value="{{ request('group_id') }}">
     <select name="sort_by" id="sort_by">
         <option value="price" {{ request('sort_by') === 'price' ? 'selected' : '' }}>Цена</option>
         <option value="name" {{ request('sort_by') === 'name' ? 'selected' : '' }}>Название</option>
+    </select>
+    <label for="sort_by">Порядок:</label>
+    <select name="order" id="order">
+        <option value="asc" {{ request('sort_by') === 'request' ? 'selected' : '' }}>Возрастанию</option>
+        <option value="desc" {{ request('sort_by') === 'request' ? 'selected' : '' }}>Убыванию</option>
     </select>
     <button type="submit">Применить</button>
 </form>
